42458c72d9647ebbf5f39cdbd6d5532a3ba03654
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-12-10 09:46:21 -0300

    lista adj done

--- src/main.c
+++ src/main.c
@@ -35,14 +35,125 @@ GraphMatrix* create_graph_matrix(int n) {
     return graph;
 }
 
+double search_matrix(GraphMatrix* matrixGraph, int id_city1, int id_city2) {
+    //Eu posso adicionar uma verificação pra ver se os ids são válidos, se for inválido retorna -1 (afinal n existe distância negativa)
+    return matrixGraph->distances[id_city1][id_city2];
+}
+
+void changeDist_matrix(GraphMatrix* matrixGraph, int id_city1, int id_city2, double distance) {
+    matrixGraph->distances[id_city1][id_city2] = distance;
+    return;
+}
+
+void insert_matrix(GraphMatrix* matrixGraph, City** cities_ptr, City new_city) {
+    
+    //Coloca a nova cidade dentro do vetor de cities
+    
+    *cities_ptr = (City*)realloc(*cities_ptr, sizeof(City) * (matrixGraph->num_cities + 1));
+    
+    if ((*cities_ptr) == NULL) {
+        printf("Erro ao realocar vetor de cidades");
+        exit(1);
+    }
+
+    (*cities_ptr)[matrixGraph->num_cities] = new_city;
+
+    int old_n = matrixGraph->num_cities;
+    int new_n = matrixGraph->num_cities + 1;
+    
+    double** new_distances = (double**) realloc(matrixGraph->distances, new_n * sizeof(double*));
+
+    if (new_distances == NULL) {
+        printf("Realocação falhou!\n");
+        exit(1);
+    }
+
+    matrixGraph->distances = new_distances;
+
+    //Realocando só as colunas que existem (a nova vai precisar de malloc)
+    for(int i = 0; i < old_n; i++) {
+        // realoca as colunas
+        matrixGraph->distances[i] = (double*)realloc(matrixGraph->distances[i], new_n * sizeof(double));
+        
+        //n sei se é necessário fazer esse if. Pensando em performance
+        if (new_distances[i] == NULL) {
+            printf("Realocação falhou!\n");
+            exit(1);
+        }
+        
+    }
+    //Realocando a nova coluna
+    new_distances[old_n] = (double*)malloc(new_n * sizeof(double));
+
+    // Recalcular a distância
+    for(int i = 0; i < new_n; i++) {
+        // 0/3 e o 3/0.
+        // 1/3 e o 3/1.
+        if(old_n == i) {
+            matrixGraph->distances[old_n][i] = 0.0;
+        } else {
+            double dist = distance(new_city, (*cities_ptr)[i]);
+            matrixGraph->distances[old_n][i] = dist;
+            matrixGraph->distances[i][old_n] = dist;
+        }
+
+    }
+
+    matrixGraph->num_cities = new_n;
+    return;
+
+}
+
+void remove_matrix(GraphMatrix* matrixGraph, int city_id, City** cities_ptr) {
+    int remove_index = city_id - 1;
+    int n = matrixGraph->num_cities;
+
+    // Libera a linha que vamos revoler
+    free(matrixGraph->distances[remove_index]);
+
+    //puxa todas as linhas 1 "pra cima" com os espaços
+    for(int i = remove_index; i < n - 1 ; i++) {
+        matrixGraph->distances[i] = matrixGraph->distances[i + 1];
+    }   
+
+    //puxa todas as colunas 1 "pro lado"
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            matrixGraph->distances[i][j] = matrixGraph->distances[i][j+1];
+        }
+    }
+
+    //Realloc pra liberar a memória extra que ficou
+    matrixGraph->distances = (double**)realloc(matrixGraph->distances, (n-1) * sizeof(double*));
+
+    for (int i = 0; i < n - 1; i++) {
+        matrixGraph->distances[i] = (double*)realloc(matrixGraph->distances[i], (n - 1) * sizeof(double));
+    }
+    
+    //Shift de tudo no cities pra esquerda, cobrindo o buraco da remoção
+    for (int i = remove_index; i < n - 1; i++) {
+        //Usamos ponteiro pra ponteiro pra mudar a estrutura original
+        (*cities_ptr)[i] = (*cities_ptr)[i+1];
+    }
+
+    // Atualiza o ponteiro original do cities, pois o realloc que limpa o espaço pode mudar o endereço
+    *cities_ptr = (City*)realloc(*cities_ptr, sizeof(City) * n - 1);
+    
+    matrixGraph->num_cities--;
+}
 
 int main () {
     City* cities;
     bool isCoord = false;
     int i = 0;
     char line[500];
-    FILE* fp = fopen("ja9847.tsp", "r");
+    FILE* fp = fopen("ja_tests.tsp", "r");
     int dimension = 0;
+
+    if (fp == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        exit(1);
+    }
 
     // Lê linha por linha do arquivo
     while(fgets(line, sizeof(line), fp) != NULL) {
@@ -56,9 +167,9 @@ int main () {
             if(sscanf(line, "%d %lf %lf", &cities[i].id, &cities[i].x, &cities[i].y) != 3) {
                 printf("Erro ao ler a coordenada da %da cidade!\n", i);
             } else {
-                printf("Cidade %d: x: %.4lf y: %.4lf\n", cities[i].id, cities[i].x, cities[i].y);
+                // printf("Cidade %d: x: %.4lf y: %.4lf\n", cities[i].id, cities[i].x, cities[i].y);
+                i++;
             }
-            i++;
         }
         
         // Pega quantas cidades existem no arquivo
@@ -77,16 +188,18 @@ int main () {
             //dps daqui vamos começar a ler as coordenadas, ent vamo setar uma flag pra isso e alocar o vetor
             cities = (City*)malloc(sizeof(City) * dimension);
             isCoord = true;
+            continue;
         }
     }
     fclose(fp);
     // Teste pra ver se deu certo
-    printf("Cidade %d: x: %.4f y: %.4f\n", cities[0].id, cities[0].x, cities[0].y);
+    // printf("Cidade %d: x: %.4f y: %.4f\n", cities[0].id, cities[0].x, cities[0].y);
 
     // Início da parte que cria a matriz de adjacência
 
     GraphMatrix* matrixGraph = create_graph_matrix(dimension);
 
+    // Preenche a matriz de adjacência
     for (int i = 0; i < dimension; i++) {
         for (int j = 0; j < dimension; j++) {
             if (i == j) {
@@ -98,5 +211,37 @@ int main () {
         }
     }
 
+
+    
+    // TESTES
+    int c1 = cities[0].id - 1, c2 = cities[1].id - 1;
+
+    printf("Acesso a matriz de adjacencia:\nDistancia entre cidade %d e cidade %d: %.4f\n", c1, c2, search_matrix(matrixGraph, c1, c2));
+    printf("Mudando distância para 45.00...\n\n");
+
+    changeDist_matrix(matrixGraph, c1, c2, 45.00);
+    printf("Acesso a matriz de adjacencia:\nDistancia entre cidade %d e cidade %d: %.4f", c1, c2, search_matrix(matrixGraph, c1, c2));
+
+    City new_city;
+    new_city.id = matrixGraph->num_cities + 1;
+    new_city.x = 24000.00;
+    new_city.y = 15000.00;
+
+    insert_matrix(matrixGraph, &cities, new_city);
+
+    c1 = 0, c2 = matrixGraph->num_cities - 1;
+    printf("Acesso a matriz de adjacencia:\nDistancia entre cidade %d e cidade %d: %.4f\n", c1, c2, search_matrix(matrixGraph, c1, c2));
+    printf("Mudando distância para 95.00...\n\n");
+
+    changeDist_matrix(matrixGraph, c1, c2, 95.00);
+    printf("Acesso a matriz de adjacencia:\nDistancia entre cidade %d e cidade %d: %.4f\n", c1, c2, search_matrix(matrixGraph, c1, c2));
+
+    printf("Removendo a cidade 10\n");
+    remove_matrix(matrixGraph, cities[11].id, &cities);
+
+    printf("%d", search_matrix(matrixGraph, 11, 0)); //isso aqui vai dar erro ou imprimir algo fodase
+
+
+    
 
 }
